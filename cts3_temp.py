415f03202f7e202aa2f8535dc0e878443faad948
thy-amigo <thy-amigo@users.noreply.github.com>
2022-08-30 12:57:08 +0530

    V1.1.1
    > Added Trace Lookup and GW Debug Option
    > Changed theme of the Website
    > Removed Streamlit and menu from webpage

--- cts3_temp.py
+++ cts3_temp.py
@@ -7,6 +7,13 @@ Created on Mon Aug 22 17:52:54 2022
 
 import streamlit as st
 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 def gen(info_dic):
        
@@ -40,36 +47,58 @@ def gen(info_dic):
     
     return template
         
+# ADD SIDEBAR OPTIONS
+with st.sidebar:
+    mode = st.selectbox('SELECT OPTION',
+                        ('CTS3 Template', 'Trace Lookup', 'GW Debug'))
+
+if mode == 'CTS3 Template':
+  
+    st.header('CTS3 TEMPLATE')
     
-st.header('CTS3 TEMPLATE')
-
-pd = st.text_area(label='Problem Description', height = 100)
-
-cf = st.text_area(label='Call Flow', height = 100)
-
-ud = st.text_area(label='User Details', height = 100)
-
-dd = st.text_area(label='Device Details', height = 100)
-
-td = st.text_area(label='Troubleshooting Done / Case History', height = 100)
-
-info_dic = {'Problem Description': pd,
-            'Call Flow': cf,
-            'User Details': ud,
-            'Device Details': dd,
-            'Troubleshooting Done / Case History': td}
-
-# st.button(label='GENERATE')
-
-if st.button(label='GENERATE'):
+    pd = st.text_area(label='Problem Description', height = 100)
     
+    cf = st.text_area(label='Call Flow', height = 100)
     
-    temp = gen(info_dic)
+    ud = st.text_area(label='User Details', height = 100)
     
-    s = ''
+    dd = st.text_area(label='Device Details', height = 100)
     
-    for i in temp:
+    td = st.text_area(label='Troubleshooting Done / Case History', height = 100)
+    
+    info_dic = {'Problem Description': pd,
+                'Call Flow': cf,
+                'User Details': ud,
+                'Device Details': dd,
+                'Troubleshooting Done / Case History': td}
+    
+    # st.button(label='GENERATE')
+    
+    if st.button(label='GENERATE'):
         
-        s += i+'\n'
         
-    st.text_area(label ="",value=s, height=1000)
+        temp = gen(info_dic)
+        
+        s = ''
+        
+        for i in temp:
+            
+            s += i+'\n'
+            
+        st.text_area(label ="",value=s, height=1000)
+
+elif mode == 'Trace Lookup':
+    st.header('TRACE LOOKUP')
+    
+    options = ('CUCM',
+               'Voice Gateway',
+               'Unity Connection',
+               'UCCX',
+               'UCCE',
+               'IM&P',
+               'Expressway C & E',
+               'Packet capture procedure',
+               'CUEAC')
+    st.subheader('Select Server/Device:')
+    st.selectbox('',options = options)
+    
