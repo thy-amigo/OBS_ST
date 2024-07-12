9ff3a1476d341a110f0fffbf2a30b9812259a7ab
thy-amigo <ipnetworking99@gmail.com>
2024-07-12 15:05:26 +0530

    V1.1
    -FixED Jabber Log parser

--- cts3_temp.py
+++ cts3_temp.py
@@ -181,8 +181,8 @@ def search_for_jabber_log(directory):
     for root, dirs, files in os.walk(directory):
         # for filename in fnmatch.filter(files, 'Jabber.log'):
         for name in files:
-            st.write('Filename: ', name)
-            st.write('Root: ', root)
+            # st.write('Filename: ', name)
+            # st.write('Root: ', root)
             if name == 'jabber.log':
                 jabber_log_path = os.path.join(root, name)
                 print(f"Found Jabber.log at: {jabber_log_path}")
@@ -2825,14 +2825,8 @@ elif mode == "Jabber Log Parser":
         # Extract the zip file
         extract_zip(uploaded_file, temp_dir)
 
-        # for root, dirs, files in os.walk(temp_dir, topdown=False):
-        #     for name in files:
-        #         st.write(os.path.join(root, name))
-        #     for name in dirs:
-        #         st.write(os.path.join(root, name))
-
         log_directory = search_for_jabber_log(temp_dir)
-        st.write('Jabber Log path: ', log_directory)
+        # st.write('Jabber Log path: ', log_directory)
         
         request_num, request_dic, nodes, summary = parse_logs(log_directory)
         
